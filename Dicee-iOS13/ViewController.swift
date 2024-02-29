949b8606ec17ee5129dcc753fdebb1faf23e933d
Kruize Derek Christensen <christensen.kruize@gmail.com>
2024-02-29 10:38:57 -0700

    Add files via upload

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageViewLeft: UIImageView!
+    @IBOutlet weak var diceImageViewRight: UIImageView!
+    
+    let dice = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Roll Button Pressed")
+        diceImageViewLeft.image = dice.randomElement()!
+        diceImageViewRight.image = dice.randomElement()!
+
+    }
+    
+}
+
